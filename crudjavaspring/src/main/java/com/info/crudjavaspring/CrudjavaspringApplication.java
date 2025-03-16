a2149614ebc1badeff04a4167d2f9d7a8f5a15e5
gstvcolaco <gustavocolaco2014@gmail.com>
2025-03-16 20:06:35 -0300

    extraçao do .zip

---
+++ crudjavaspring/src/main/java/com/info/crudjavaspring/CrudjavaspringApplication.java
@@ -0,0 +1,13 @@
+package com.info.crudjavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudjavaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudjavaspringApplication.class, args);
+	}
+
+}
---
+++ crudjavaspring/src/test/java/com/info/crudjavaspring/CrudjavaspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.info.crudjavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudjavaspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
